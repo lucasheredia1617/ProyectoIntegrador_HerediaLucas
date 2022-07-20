8186bbd731c610ad4d51c9cd474117c4b28cdbbc
lucasheredia1617 <lucasheredia5475@gmail.com>
2022-07-19 23:54:40 -0300

    Todo el FrontEnd y cambio de carpeta

---
+++ ProyectoIntegrador_HerediaLucas/FrontEnd/src/app/components/hardandsoftskilss/hardandsoftskilss.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hardandsoftskilss',
+  templateUrl: './hardandsoftskilss.component.html',
+  styleUrls: ['./hardandsoftskilss.component.css']
+})
+export class HardandsoftskilssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
